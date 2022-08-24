d61f5ed812908020094e3a8ffa07217018df8a21
sakthi-sakthi <110651198+sakthi-sakthi@users.noreply.github.com>
2022-08-24 21:57:47 +0530

    Update display.php

--- display.php
+++ display.php
@@ -87,7 +87,7 @@ session_destroy();
                     if(isset($_SESSION['status']))
                     {
                         ?>
-                        <div class="alert alert-danger alert-dismissible fade show " role="alert">
+                        <div class="alert alert-success alert-dismissible fade show " role="alert">
                           <strong>Hey ! </strong> <?php echo $_SESSION['status'];?>
                           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
@@ -100,7 +100,7 @@ session_destroy();
                     if(isset($_SESSION['status']))
                     {
                         ?>
-                        <div class="alert alert-danger alert-dismissible fade show " role="alert">
+                        <div class="alert alert-success alert-dismissible fade show " role="alert">
                           <strong>Hey ! </strong> <?php echo $_SESSION['status'];?>
                           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
