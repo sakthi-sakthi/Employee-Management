6b317d9cfb1326ccb6e1e305a82f4a94f09d7ec3
sakthi-sakthi <110651198+sakthi-sakthi@users.noreply.github.com>
2022-08-24 21:50:08 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if(!isset($_SERVER['HTTP_REFERER']))
+{    
+    header('location:/Employee-Management/index.php');
+    exit;
+}
 session_destroy();
 {
 	header('location:http://localhost/Employee-Management/index.php');
